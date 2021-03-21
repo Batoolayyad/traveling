817aa60c20b4b22b2c8bf9b15391fed7de2f9dab
Batoolayyad <batoolayyad1996@gmail.com>
2021-03-21 14:50:51 +0200

    adding a loop

--- web.js
+++ web.js
@@ -1,23 +1,49 @@
 //welcoming alert
 
-var userName= prompt("inter your name please");
-alert ('Hello '+ userName)
+//var userName= prompt("inter your name please");
+alert ('Hello ')
 
 
 //asking the web visitor about his fevorite city 
-
-//show a pic of the city that has been choose
-
 var favoriteCity= prompt("what is your favorite city between Dubai, Brcelona and Istanbul?");
 
-if(favoriteCity=='Dubai'){document.write('<img src="https://www.aljazeera.net/wp-content/uploads/2014/11/a7f71861-8c3d-47cd-ba34-50b96aa010a7.jpeg?resize=686%2C513" width="300px" height="150px"/>');
-}
-else if (favoriteCity=='Brcelona') {document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYoEyQB0Jk_kHDFrVYozY3KPXDRhnNCEFQBA&usqp=CAU" width="300px" height="150px"/>'); 
-}
-else if (favoriteCity=='Istanbul'){document.write('<img src="https://www.nomadepicureans.com/wp-content/uploads/2019/09/One-Day-in-Istanbul-Feature-Image.jpg" width="300px" height="150px"/>');
-    
-} 
-else{ alert("choose between the cites that are showing");
+
+//keep asking until the visitor enter one of the chooises
+while((favoriteCity!='Dubai')&&(favoriteCity!='Brcelona')&&(favoriteCity!='Istanbul')){
+    favoriteCity=prompt("please select between the cities that have been mention");
 }
 
-document.write('<h3>this is your favorite city ' + favoriteCity +'</h3>');
+//show a pic of the city that has been choose
+var cityPic=" "
+var result=" "
+
+
+if(favoriteCity=='Dubai'){
+    cityPic='<img src="https://www.aljazeera.net/wp-content/uploads/2014/11/a7f71861-8c3d-47cd-ba34-50b96aa010a7.jpeg?resize=686%2C513" width="300px" height="150px"/>';
+}
+else if (favoriteCity=='Brcelona') {
+    cityPic='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYoEyQB0Jk_kHDFrVYozY3KPXDRhnNCEFQBA&usqp=CAU" width="300px" height="150px"/>'; 
+}
+else if (favoriteCity=='Istanbul'){
+    cityPic='<img src="https://www.nomadepicureans.com/wp-content/uploads/2019/09/One-Day-in-Istanbul-Feature-Image.jpg" width="300px" height="150px"/>';
+    
+} 
+
+document.write('<h3>here is your favorite city ' + favoriteCity +'</h3>');
+
+
+//for loop to choose how many pictures the visitor want the pic to show 
+var numberOfPic= prompt('how many pic you want to show?');
+
+
+for(i=0;i< numberOfPic; i++){
+  result+=cityPic;
+}
+document.write(result);
+
+//else{ alert("choose between the cites that are showing");}
+
+// write the favorite city name for the visitor under the pic
+
+
+
